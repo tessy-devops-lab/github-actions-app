864385091368b7d749e6290a468dfb1c7190c8bf
Tessy666 <tessyeleazer@gmail.com>
2026-09-02 12:33:25 +0530

    Restore healthy application

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function greet(name) {
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.destroy();
+  res.end("Hello Tessy! GitHub Actions + Docker + ECR is working!\n");
 });
 
 if (require.main === module) {
